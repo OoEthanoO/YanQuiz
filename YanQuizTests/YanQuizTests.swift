5ed13296904c5d21e4adcb9d8e7a3646781649fe
Ethan Xu <ethanxucoder@gmail.com>
2025-04-19 19:19:05 -0400

    Initial Commit

---
+++ YanQuizTests/YanQuizTests.swift
@@ -0,0 +1,17 @@
+//
+//  YanQuizTests.swift
+//  YanQuizTests
+//
+//  Created by Ethan Xu on 2025-04-19.
+//
+
+import Testing
+@testable import YanQuiz
+
+struct YanQuizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
